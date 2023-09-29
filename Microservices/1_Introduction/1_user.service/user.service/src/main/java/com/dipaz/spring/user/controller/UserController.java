f3db58b01bd2363e782860d477a80a1865e28938
diego40g <diegopaznaula@hotmail.com>
2023-09-28 23:30:41 -0500

    try with other command for manager routes

--- Microservices/1_Introduction/1_user.service/user.service/src/main/java/com/dipaz/spring/user/controller/UserController.java
+++ Microservices/1_Introduction/1_user.service/user.service/src/main/java/com/dipaz/spring/user/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.dipaz.spring.user.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping("/mi-ruta")
 public class UserController {
-    @GetMapping("/greetings")
+    @GetMapping("/nueva-ruta")
     public String greetings() {
-        return "Hello World!!!";
+        return "Hello User!!!";
     }
 }
