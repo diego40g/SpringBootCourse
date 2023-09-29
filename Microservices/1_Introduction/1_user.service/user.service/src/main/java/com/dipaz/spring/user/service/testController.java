6b1507a6af2d533b039526d89f6f24b604c1e887
diego40g <diegopaznaula@hotmail.com>
2023-09-28 23:28:08 -0500

    adding a test controller for learn routes

--- Microservices/1_Introduction/1_user.service/user.service/src/main/java/com/dipaz/spring/user/service/testController.java
+++ Microservices/1_Introduction/1_user.service/user.service/src/main/java/com/dipaz/spring/user/service/testController.java
@@ -1,11 +1,10 @@
-package com.dipaz.spring.user.controller;
+package com.dipaz.spring.user.service;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class UserController {
+public class testController {
     @GetMapping("/greetings")
     public String greetings() {
         return "Hello World!!!";
---
+++ Microservices/1_Introduction/2_user_service_8/Service/src/main/java/com/dipaz/spring/user/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.dipaz.spring.user.controller;
+
+public class UserController {
+}
