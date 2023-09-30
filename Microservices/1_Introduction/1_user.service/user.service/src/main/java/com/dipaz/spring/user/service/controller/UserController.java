f4add0bc586a1dd9955e5bfa276bc6acaaa92fb0
diego40g <diegopaznaula@hotmail.com>
2023-09-29 22:22:21 -0500

    move packages to manager routes

--- Microservices/1_Introduction/1_user.service/user.service/src/main/java/com/dipaz/spring/user/service/controller/UserController.java
+++ Microservices/1_Introduction/1_user.service/user.service/src/main/java/com/dipaz/spring/user/service/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.dipaz.spring.user.controller;
+package com.dipaz.spring.user.service.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
