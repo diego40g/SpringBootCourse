e8ccf400bf5adefe51088844838782d2fa5dd655
diego40g <diegopaznaula@hotmail.com>
2023-09-30 20:23:02 -0500

    a new default spring project

---
+++ Microservices/1_Introduction/OnJava8/2_externalController/Service/src/main/java/com/dipaz/spring/user/service/ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dipaz.spring.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+}
