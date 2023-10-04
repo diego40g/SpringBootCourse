e3a206f6828f828210e7a2264f6886c9c826e6ef
diego40g <diegopaznaula@hotmail.com>
2023-10-03 23:22:48 -0500

    adding controller user to test path urls

---
+++ Microservices/1_Introduction/4_configDB/user.service/src/main/java/com/dipaz/spring/user/service/UserController.java
@@ -0,0 +1,13 @@
+package com.dipaz.spring.user.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @RequestMapping(path = "saludo", method = RequestMethod.GET)
+    public String saludo(){
+        return "Hola";
+    }
+}
