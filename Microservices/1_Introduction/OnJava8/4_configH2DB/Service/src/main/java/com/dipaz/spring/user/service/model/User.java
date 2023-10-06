ad5918258714092b614070eb6d70b49d4806908f
diego40g <diegopaznaula@hotmail.com>
2023-10-05 22:19:29 -0500

    adding a class User to add on database

---
+++ Microservices/1_Introduction/OnJava8/4_configH2DB/Service/src/main/java/com/dipaz/spring/user/service/model/User.java
@@ -0,0 +1,54 @@
+package com.dipaz.spring.user.service.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public User() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
