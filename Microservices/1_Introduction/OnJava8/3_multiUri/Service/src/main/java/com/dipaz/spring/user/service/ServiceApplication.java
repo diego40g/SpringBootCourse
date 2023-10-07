9712a1d993af8e8a4bd6503f9a87ab33b88bc765
diego40g <diegopaznaula@hotmail.com>
2023-10-06 23:25:24 -0500

    import component controller to use in main fucntions

--- Microservices/1_Introduction/OnJava8/3_multiUri/Service/src/main/java/com/dipaz/spring/user/service/ServiceApplication.java
+++ Microservices/1_Introduction/OnJava8/3_multiUri/Service/src/main/java/com/dipaz/spring/user/service/ServiceApplication.java
@@ -2,8 +2,10 @@ package com.dipaz.spring.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.dipaz.spring.user.controller"})
 public class ServiceApplication {
 
 	public static void main(String[] args) {
