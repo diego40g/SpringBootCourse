24201ff9332a87b38ec7bffa653d298c56b782aa
diego40g <diegopaznaula@hotmail.com>
2023-10-09 23:37:27 -0500

    updating user model

---
+++ Microservices/1_Introduction/OnJava21/5_entityExample/user.service/src/main/java/com/dipaz/spring/user/service/user/service/model/User.java
@@ -0,0 +1,67 @@
+package com.dipaz.spring.user.service.user.service.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="usuarios")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "age")
+    private int age;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastname, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- Microservices/1_Introduction/OnJava8/4_configH2DB/Service/src/main/java/com/dipaz/spring/user/service/model/User.java
+++ Microservices/1_Introduction/OnJava8/4_configH2DB/Service/src/main/java/com/dipaz/spring/user/service/model/User.java
@@ -1,17 +1,18 @@
 package com.dipaz.spring.user.service.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.io.Serializable;
 
 @Entity
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long userId;
     private String firstname;
     private String lastname;
     private String email;
@@ -21,11 +22,11 @@ public class User implements Serializable {
     }
 
     public Long getId() {
-        return id;
+        return userId;
     }
 
     public void setId(Long id) {
-        this.id = id;
+        this.userId = id;
     }
 
     public String getFirstname() {
