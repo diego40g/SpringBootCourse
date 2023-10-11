fff7aa4cc0db822a17aa98c4889661f18d501081
diego40g <diegopaznaula@hotmail.com>
2023-10-10 19:18:26 -0500

    adding a new empty project of spring booy

---
+++ Microservices/1_Introduction/OnJava21/6_jpaExample/user.service/src/test/java/com/dipaz/spring/user/service/user/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipaz.spring.user.service.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
