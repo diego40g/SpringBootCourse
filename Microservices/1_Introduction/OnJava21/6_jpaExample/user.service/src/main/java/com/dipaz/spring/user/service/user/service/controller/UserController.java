bd492fdd400bcf76b0d0c2c59d4afcccbeb3e542
diego40g <diegopaznaula@hotmail.com>
2023-10-11 22:48:15 -0500

    adding get all users

--- Microservices/1_Introduction/OnJava21/6_jpaExample/user.service/src/main/java/com/dipaz/spring/user/service/user/service/controller/UserController.java
+++ Microservices/1_Introduction/OnJava21/6_jpaExample/user.service/src/main/java/com/dipaz/spring/user/service/user/service/controller/UserController.java
@@ -4,11 +4,12 @@ import com.dipaz.spring.user.service.user.service.model.User;
 import com.dipaz.spring.user.service.user.service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -25,8 +26,8 @@ public class UserController {
         User createUser=userRepository.save(user);
         return new ResponseEntity<>(createUser, HttpStatus.CREATED);
     }
-    @PostMapping("/tutorials")
-    public ResponseEntity<User> createTutorial(@RequestBody User user) {
+    @PostMapping("/add/user")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         try {
             User _user = userRepository.save(new User(user.getFirstname(), user.getLastname(), user.getEmail(),true));
             return new ResponseEntity<>(_user, HttpStatus.CREATED);
@@ -46,7 +47,7 @@ public class UserController {
     }
 
     @GetMapping("/user/res/{id}")
-    public ResponseEntity<User> getTutorialById(@PathVariable("id") long id) {
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
         Optional<User> userData = userRepository.findById(id);
 
         if (userData.isPresent()) {
@@ -56,4 +57,23 @@ public class UserController {
         }
     }
 
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(@RequestParam(required = false) String title) {
+        try {
+            List<User> users = new ArrayList<User>();
+
+            if (title == null)
+                userRepository.findAll().forEach(users::add);
+            else
+                userRepository.findByEmailContainingIgnoreCase(title).forEach(users::add);
+
+            if (users.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
