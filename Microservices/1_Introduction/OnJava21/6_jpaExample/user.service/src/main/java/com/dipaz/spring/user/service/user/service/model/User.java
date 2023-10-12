9391edbfb796e83cf6eb7d647f30348d557919b3
diego40g <diegopaznaula@hotmail.com>
2023-10-11 22:48:44 -0500

    adding super on constructor and getters and setters

--- Microservices/1_Introduction/OnJava21/6_jpaExample/user.service/src/main/java/com/dipaz/spring/user/service/user/service/model/User.java
+++ Microservices/1_Introduction/OnJava21/6_jpaExample/user.service/src/main/java/com/dipaz/spring/user/service/user/service/model/User.java
@@ -18,5 +18,45 @@ public class User implements Serializable {
     @Column(name = "active")
     private boolean active;
     public User() {
+        super();
+    }
+
+    public User(String firstname, String lastname, String email, boolean active) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.active = active;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
