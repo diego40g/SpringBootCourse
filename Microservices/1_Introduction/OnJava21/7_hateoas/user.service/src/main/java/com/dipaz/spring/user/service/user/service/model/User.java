e6c21bf277af8fa7d7dd0e6eb35082a2a9f281ed
diego40g <diegopaznaula@hotmail.com>
2023-11-02 23:48:12 -0500

    in old version de resourceSupport was delete

--- Microservices/1_Introduction/OnJava21/7_hateoas/user.service/src/main/java/com/dipaz/spring/user/service/user/service/model/User.java
+++ Microservices/1_Introduction/OnJava21/7_hateoas/user.service/src/main/java/com/dipaz/spring/user/service/user/service/model/User.java
@@ -1,11 +1,12 @@
 package com.dipaz.spring.user.service.user.service.model;
 
 import java.io.Serializable;
+
 import jakarta.persistence.*;
 
 @Entity
 @Table(name="usuarios")
-public class User extends ResourceSupport implements Serializable {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
