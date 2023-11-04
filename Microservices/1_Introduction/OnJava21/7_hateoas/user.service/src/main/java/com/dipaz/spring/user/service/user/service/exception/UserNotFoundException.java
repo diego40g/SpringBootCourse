3c7eae6de156caf6d7e61763ccc1e72e6a800e33
diego40g <diegopaznaula@hotmail.com>
2023-11-03 23:47:21 -0500

    adding a user not found exception

---
+++ Microservices/1_Introduction/OnJava21/7_hateoas/user.service/src/main/java/com/dipaz/spring/user/service/user/service/exception/UserNotFoundException.java
@@ -0,0 +1,18 @@
+package com.dipaz.spring.user.service.user.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends Exception{
+    private static final long serialVersionUID = 1L;
+    private String message;
+
+    public UserNotFoundException(String message) {
+        super();
+        this.message = message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
